d3a7da14326852c2e80015a3bdea4ebdcc0edf72
TheShubham-K <subham.kumar032@gmail.com>
2021-10-30 19:36:00 +0530

    initial release

--- calculate/calculatorapp/views.py
+++ calculate/calculatorapp/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 import math
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
-
+@login_required
 def api(request):
     return render(request, 'calculator.html')
 
